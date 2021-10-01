6985c6122cd3207ca41ac906aad14d9201bc2f35
Fabiano Garcia <fabianojp06@gmail.com>
2021-10-01 09:34:08 -0300

    criado parametros de enviroments

--- src/modules/board/controller.js
+++ src/modules/board/controller.js
@@ -3,7 +3,6 @@ import { createBoard, listBoards, getBoard, updatedBoard, deleteBoard } from './
 
 export const create = async (event) => {
     try{
-        console.log("creating board...")
        const auth = event.requestContext.authorizer
        const body = JSON.parse(event.body)
        const board = await createBoard(auth, body) 
